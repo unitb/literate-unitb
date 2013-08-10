e625d341695416ff99f93c3d40bb22a147c542de
Simon <simon.hudon@gmail.com>
2013-08-10 03:09:11 -0400

    refactored continuous to use the StateT monad transformer instead of threading program arguments and proof obligations through all the functions.
    Additionally, the cyclic executive has been factored into a
    parameterized fonction.

--- continuous.hs
+++ continuous.hs
@@ -2,8 +2,13 @@ module Main where
 
 import Control.Concurrent
 import Control.Monad
+import Control.Monad.State
 
-import Data.Map as M (lookup,empty,union,fromList)
+import Data.Map as M 
+    ( Map,lookup
+    , empty,union
+    , fromList,insert 
+    )
 import Document.Document
 
 import System.Console.GetOpt
@@ -17,40 +22,64 @@ import Text.Printf
 import UnitB.AST
 import UnitB.PO
 
+import Z3.Z3
+
+monitor :: (Monad m, Eq s) 
+        => m s -> m () 
+        -> m () -> m ()
+monitor measure delay act = do
+    t <- measure
+    runStateT (
+        forever (do
+            t0 <- get
+            t1 <- lift measure
+            if t0 == t1
+                then return ()
+                else do 
+                    put t1
+                    lift act
+            lift delay)) t
+    return ()
+
 data Params = Params
         { path :: FilePath
         , verbose :: Bool
         , continuous :: Bool
+        , pos :: Map Label (Map Label (Bool,ProofObligation))
         }
 
-check_one pos m = do
-        let { po = case M.lookup (_name m) pos of
-            Just po -> po
-            Nothing -> empty }
-        (po,s,n)    <- verify_changes m po
-        return ((n,"> machine " ++ show (_name m) ++ ":\n" ++ s), (_name m,po))
+check_one m = do
+        param <- get
+        let p = M.lookup (_name m) $ pos param
+        let po = maybe empty id p
+        (po,s,n)    <- lift $ verify_changes m po
+        put (param { pos = insert (_name m) po $ pos param })
+        return (n,"> machine " ++ show (_name m) ++ ":\n" ++ s)
 
-check_file param m = do
-        r <- parse_machine $ path param
+clear = do
+    param <- get
+    if continuous param 
+        then lift $ putStr $ take 40 $ cycle "\n"
+        else return ()
+
+check_file = do
+        param <- get
+        let m = pos param
+        let { p ln = verbose param || take 4 ln /= "  o " }
+--        let f (n,xs) = unlines (filter p (lines xs) ++ ["Redid " ++ show n ++ " proofs"])
+        r <- lift $ parse_machine $ path param
         case r of
             Right ms -> do
-                xs <- forM ms (check_one m)
-                if continuous param then
-                    putStr $ take 40 $ cycle "\n"
-                else return ()
-                forM_ (map fst xs) (putStrLn . f)
-                return (union (fromList $ map snd xs) m)
+                xs <- forM ms check_one
+                clear
+                forM_ xs (\(n,xs) -> lift $ do
+                    forM_ (filter p $ lines xs) 
+                        putStrLn
+                    putStrLn ("Redid " ++ show n ++ " proofs"))
             Left xs -> do
-                if continuous param then
-                    putStr $ take 40 $ cycle "\n"
-                else return ()
-                forM_ xs (\(x,i,j) -> printf "error (%d,%d): %s\n" i j x)
-                return m
-    where
-        f (n,xs) = unlines (filter p (lines xs) ++ ["Redid " ++ show n ++ " proofs"])
-        p ln = 
-            if verbose param then True
-            else take 4 ln /= "  o "
+                clear
+                lift $ forM_ xs (\(x,i,j) -> 
+                    printf "error (%d,%d): %s\n" i j x)
 
 data Option = Verbose | Continuous
     deriving Eq
@@ -69,30 +98,22 @@ main = do
         case args of
             [xs] -> do
                 b <- doesFileExist xs
-                if b
-                then do
+                if b then do
                     let { param = Params 
-                        { path = xs
-                        , verbose = Verbose `elem` opts
-                        , continuous = Continuous `elem` opts
-                        } }
-                    pos <- check_file param empty
-                    if Continuous `elem` opts then do
-                        t <- getModificationTime xs
-                        f param xs (t, pos) 
-                    else return ()
+                            { path = xs
+                            , verbose = Verbose `elem` opts
+                            , continuous = Continuous `elem` opts
+                            , pos = empty
+                            } }
+                    runStateT (do
+                        check_file
+                        if Continuous `elem` opts then do
+                            monitor
+                                (lift $ getModificationTime xs)
+                                (lift $ threadDelay 1000000)
+                                check_file
+                        else return ()) param
+                    return ()
                 else do
                     putStrLn ("'" ++ xs ++ "' is not a valid file")
             _ -> putStrLn $ usageInfo "usage: continuous file" options
-    where
-        f param xs (t0,m) = do
-            threadDelay 100000
-            x <- hReady stdin
-            c <- if x then getChar else return ' '
-            if c `elem` ['q','Q'] then return ()
-            else do 
-                t1 <- getModificationTime xs
-                m <- if t0 /= t1 
-                    then check_file param m 
-                    else return m
-                f param xs (t1,m) 
