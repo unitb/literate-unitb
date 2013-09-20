95886cff1810b7ba07b6ee2bab5590f71e860259
Simon <simon.hudon@gmail.com>
2013-09-19 22:55:31 -0400

    added a continuous testing program

---
+++ periodic.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Concurrent.Thread.Delay
+import Control.Monad
+
+import System.Process
+
+interval = Minutes 10
+
+data Time = Minutes Integer | Seconds Integer
+
+microseconds (Minutes x) = x * 60000000
+microseconds (Seconds x) = x * 1000000
+
+main = do
+    forever $ do
+        system "cp result.txt last_result.txt"
+        system "./run_tests"
+        delay (microseconds interval)
