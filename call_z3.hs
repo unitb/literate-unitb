ac21a59c35fb91f933394236d4d7ee2c0b07dfde
Simon Hudon <simon@cse.yorku.ca>
2014-05-11 21:25:13 -0400

    new tool for trying all the Z3 sources that failed.

---
+++ call_z3.hs
@@ -0,0 +1,19 @@
+import Control.Monad
+
+import Data.Function
+import Data.List
+
+import System.Directory
+import System.Process
+
+import Utilities.Format
+
+main = do
+        ls <- getDirectoryContents "."
+        let xs = filter (f "po-") ls
+        forM_ xs $ \fn -> do
+            putStrLn fn
+            system (format "head -n 2 {0}" fn)
+            system (format "z3 -smt2 -T:5 {0}" fn)
+    where
+        f x xs = x == take (length x) xs
