fd566fa576b5f907c33373aa9a97e8cee0e6c65a
Amin Bandali <amin@aminb.org>
2016-05-26 17:20:30 -0400

    Work on Logic.Proof.Monad
    o Change SequentM from a Writer to an RWS monad
    
    o Implement assumeQ and checkQ for use of QuasiQuotes with the proof monad
    (along with a few test cases)
    
    o Fix an issue with include that would overwrite the ParserSetting of the monad
    when including a theory.
    
    o Implement Semigroup instance for ParserSetting
    
    o Implement Semigroup and Monoid instances for Notation

--- logic/Logic/Proof/Monad.hs
+++ logic/Logic/Proof/Monad.hs
@@ -2,18 +2,23 @@ module Logic.Proof.Monad where
 
     -- Modules
 import Logic.Expr
+import Logic.Expr.Parser
+import Logic.Expr.Printable
 import Logic.Proof.Sequent
+import Logic.Theories.Arithmetic
 import Logic.Theory
 
     -- Libraries
 import Control.Lens hiding (Context)
-import Control.Monad.Writer
+import Control.Monad.RWS hiding ((<>))
 import Control.Precondition
 
 import Data.Map.Class as M
 import Data.Set as S
 
-newtype SequentM a = SequentM (Writer ([Sort],[Var],[Expr],[Context]) a)
+import Utilities.Table
+
+newtype SequentM a = SequentM (RWS () ([Sort],[Var],[Expr],[Context]) (ParserSetting,[Theory],Table Name Var) a)
     deriving (Functor,Applicative,Monad)
 
 runSequent :: SequentM Expr -> Sequent
@@ -24,10 +29,24 @@ runSequent (SequentM cmd) = empty_sequent
         & constants .~ symbol_table vs
         & context %~ merge_ctx (merge_all_ctx ctx)
     where
-        (g,(s,vs,asm,ctx)) = runWriter cmd
+        (g,(s,vs,asm,ctx)) = evalRWS cmd () (st,[arithmetic,basic_theory],M.empty)
+        st = theory_setting $ (empty_theory' "empty") { _extends = 
+                symbol_table [arithmetic,basic_theory] }
+
+updateSetting :: SequentM ()
+updateSetting = SequentM $ do
+    ts <- use _2
+    _1 .= theory_setting (empty_theory' "empty") { _extends = 
+                symbol_table ts }
+    ds <- use _3
+    _1.decls %= M.union ds
 
 include :: Theory -> SequentM ()
-include t = SequentM $ tell ([],[],M.elems $ theory_facts t,[theory_ctx t])
+include t = do
+    SequentM $ do
+        tell ([],[],M.elems $ theory_facts t,[theory_ctx t])
+        _2 %= (t:)
+    updateSetting
 
 assume :: Pre
        => ExprP -> SequentM ()
@@ -35,6 +54,12 @@ assume e = do
         let e' = fromRight' e
         collectDeclaration e'
         SequentM $ tell ([],[],[e'],[])
+
+assumeQ :: Pre
+        => (ParserSetting -> DispExpr) -> SequentM ()
+assumeQ qexpr = do
+        setting <- SequentM $ use _1
+        assume $ Right $ getExpr $ qexpr setting
 
 collectDeclaration :: Expr -> SequentM ()
 collectDeclaration e = SequentM $ do
@@ -48,10 +73,19 @@ check e = do
         collectDeclaration e'
         return e'
 
+checkQ :: Pre
+        => (ParserSetting -> DispExpr) -> SequentM Expr
+checkQ qexpr = do
+        setting <- SequentM $ use _1
+        check $ Right $ getExpr $ qexpr setting
+
 declare :: Pre
         => String -> Type -> SequentM ExprP
 declare n t = do
         let v = Var (fromString'' n) t
         collectDeclaration $ Word v
-        SequentM $ tell ([],[v],[],[])
+        SequentM $ do
+            tell ([],[v],[],[])
+            _3 %= insert_symbol (z3Var n t)
+            _1.decls %= insert_symbol (z3Var n t)
         return $ Right $ Word v
