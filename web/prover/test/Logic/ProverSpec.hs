5063a4995838c39427fcd6e1009ff91a6d168dc0
Amin Bandali <amin@aminb.org>
2016-06-07 22:23:44 -0400

    Write a new test ==> x : \Int, x = 3, x < 3 should be Invalid

--- web/prover/test/Logic/ProverSpec.hs
+++ web/prover/test/Logic/ProverSpec.hs
@@ -11,16 +11,19 @@ import Data.Maybe
 spec :: Spec
 spec = describe "prove" $ do
     it "2 = 2 should be Valid" $ do
-        (prove pf1) `shouldReturn` pr1
+        (prove pf1) `shouldReturn` prValid
 
     it "\\neg 2 = 2 should be Invalid" $ do
          shouldReturn
             (prove . justPFStr . decode $ pfStr2)
-            (justPRStr . decode $ prStr2)
+            (justPRStr . decode $ prStrInvalid)
 
     it "2 /= 2 should cause an error (not Valid or Invalid)" $ do
-        (prove pf3) `shouldNotReturn` pr1
-        (prove pf3) `shouldNotReturn` (justPRStr . decode $ prStr2)
+        (prove pf3) `shouldNotReturn` prValid
+        (prove pf3) `shouldNotReturn` (justPRStr . decode $ prStrInvalid)
+
+    it "x : \\Int, x = 3, x < 3 should be Invalid" $ do
+        (prove pf4) `shouldReturn` prInvalid
 
     where
         pf1 = ProofForm {
@@ -38,7 +41,14 @@ spec = describe "prove" $ do
             assumptions  = fromList [],
             goal         = ("2 /= 2" :: String)
         }
-        pr1    = ProofResult { result = ("Valid" :: String) }
-        prStr2 = "{\"result\":\"Invalid\"}"
+        pf4 = ProofForm {
+            theories     = fromList [],
+            declarations = fromList [("decl1", "x : \\Int" :: String)],
+            assumptions  = fromList [("asm1", ("xIs3", "x = 3"))],
+            goal         = ("x < 3" :: String)
+        }
+        prValid      = ProofResult { result = ("Valid" :: String) }
+        prStrInvalid = "{\"result\":\"Invalid\"}"
+        prInvalid    = ProofResult { result = ("Invalid" :: String) }
         justPFStr = fromJust :: Maybe (ProofForm String) -> ProofForm String
         justPRStr = fromJust :: Maybe (ProofResult String) -> ProofResult String
