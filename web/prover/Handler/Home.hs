e0efa4f8f99f818620974e4f86d9a7b3672c7ba1
Amin Bandali <amin@aminb.org>
2016-06-09 15:42:47 -0400

    Read the theory names from Logic.Theories (supportedTheories) instead of hardcoding

--- web/prover/Handler/Home.hs
+++ web/prover/Handler/Home.hs
@@ -3,6 +3,11 @@ module Handler.Home where
 import Import
 import Text.Julius (RawJS (..))
 
+import qualified Data.Char as C
+import qualified Data.Map as M
+import qualified Logic.Theories as Theories
+import Logic.Names (render)
+
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
 -- config/routes
@@ -29,3 +34,10 @@ declarationIds = ("decl", "div-decl")
 
 assumptionIds :: (Text, Text)
 assumptionIds = ("asm", "div-asm")
+
+theoryNames :: [String]
+theoryNames = render <$> M.keys Theories.supportedTheories
+
+capitalize :: String -> String
+capitalize (h:rest) = C.toUpper h : (C.toLower <$> rest)
+capitalize [] = []
