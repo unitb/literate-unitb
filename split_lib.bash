d171ce2ae9a8e9f2104b1874d303f87268dc4c1b
Simon Hudon <simon.hudon@gmail.com>
2016-10-22 16:09:31 -0400

    added caching

--- split_lib.bash
+++ split_lib.bash
@@ -1,9 +1,18 @@
-export LIB_NAME=literate-unitb-scripts
-git subtree split -P script -b simon/separate-$LIB_NAME
+export LIB_NAME=literate-unitb-config
+git subtree split -P Z3/Version.hs -b simon/separate-$LIB_NAME
 cd ..
 mkdir $LIB_NAME
 cd $LIB_NAME
 git init
-git pull ../literate-unitb simon/separate-$LIB_NAME
+git pull ../literate-unitb-logic simon/separate-$LIB_NAME
 cd ../$LIB_NAME
 stack init --omit-packages
+# export LIB_NAME=literate-unitb-scripts
+# git subtree split -P script -b simon/separate-$LIB_NAME
+# cd ..
+# mkdir $LIB_NAME
+# cd $LIB_NAME
+# git init
+# git pull ../literate-unitb simon/separate-$LIB_NAME
+# cd ../$LIB_NAME
+# stack init --omit-packages
